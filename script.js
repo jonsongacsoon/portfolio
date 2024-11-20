e1b80a85483a8866e5119d1aa717758657f28f88
jonsongacsoon <101936051+jonsongacsoon@users.noreply.github.com>
2024-11-20 23:28:28 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+// Add animations for scrolling and hovering
+document.addEventListener("scroll", () => {
+  const navbar = document.querySelector(".navbar");
+  if (window.scrollY > 50) {
+    navbar.classList.add("scrolled");
+  } else {
+    navbar.classList.remove("scrolled");
+  }
+});
